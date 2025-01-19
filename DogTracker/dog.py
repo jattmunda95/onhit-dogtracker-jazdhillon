54f97d08486b722f03c7c8a62a40407d63287240
Jaskaran Dhillon <jaskaran.dhillonz@outlook.com>
2025-01-19 23:50:08 +1100

    Initial commit of working app, with modified file structure, added classes and much more added functionality. Added classes include: - Tracker - ExerciseActivity - VetVisit - FoodEntry
    Documentation is at a minimum at this stage, future commits will have
    added documentation

--- DogTracker/dog.py
+++ DogTracker/dog.py
@@ -2,48 +2,55 @@
 Dog Class
 
 Author: Your Name
-Date: January 19, 2025,
+Date: January 19, 2025
 Description:
-    This module defines the Dog class, representing a dog with attributes such as name, breed, and age.
+    This module defines the Dog class, representing a dog with attributes such as name, breed, and gender.
     It also includes methods for managing dog profiles and tracking related information.
 
 Methods:
-    __init__(self, name, breed, age): Initializes a new Dog instance with the given name, breed, and age.
+    __init__(self, name, breed, gender): Initializes a new Dog instance with the given name, breed, and gender.
     get_all_dogs(cls): Class method to retrieve all dog instances.
     save_to_csv(cls, file_name): Class method to save all dog instances to a CSV file.
     update_name(self, new_name): Updates the dog's name.
     update_breed(self, new_breed): Updates the dog's breed.
     update_age(self, new_age): Updates the dog's age.
 """
-
-import csv
-import datetime
+import json
+from trackers.tracker import Tracker
 
 class Dog:
-
-    def __init__(self, name: str, breed: str, gender: str, date_of_birth_str: str):
+    def __init__(self, name, breed, gender):
         """
-        The initializer method for Dog class, which takes the `name`, `breed`, `gender` and `date_of_birth`
-        and assigns them to instance. Then the instance is saved to the `_all_dogs` list.
-        :param name: name of the dog
-        :param breed: breed name of the dog
-        :param gender: gender of the dog (M/F)
-        :param date_of_birth: date of birth of the dog as a string, which is taken and then converted to a datetime object
-        """
-        self._name = name
-        self._breed = breed
-        self._gender = gender
-        self._date_of_birth = None
-        with open('all_dogs.csv', 'a', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(["Name", "Breed", "Gender", "Date of Birth"])
-            writer.writerow([self._name, self._breed, self._gender, self._date_of_birth])
+        The initializer method for the Dog class, which takes the `name`, `breed`, and `gender`
+        and assigns them to the instance. Then the instance is saved to the `_all_dogs` list.
 
-    def __validate_date_of_birth(self, date_of_birth_str: str) -> datetime.datetime:
-        empty_date_string = str()
-        try:
-            datetime.datetime.strptime(date_of_birth_str, "%Y-%m-%d")
-        except ValueError:
-            print("Invalid date of birth")
-            return False
-        return datetime.datetime.strptime(date_of_birth_str, "%Y-%m-%d")
+        :param name: Name of the dog
+        :param breed: Breed name of the dog
+        :param gender: Gender of the dog (M/F)
+        """
+        self.name = name
+        self.breed = breed
+        self.gender = gender
+        self.tracker = Tracker()
+        # self.date_of_birth = None
+
+    def dog_details(self):
+        print(f"Dog Details")
+        print(f"Name: {self.name}")
+        print(f"Breed : {self.breed}")
+        print(f"Gender: {self.gender}")
+
+        self.tracker.print_all_trackers(self.name)
+
+    def to_dict(self):
+        """Convert class attributes to a dictionary."""
+        return {
+            "name": self.name,
+            "breed": self.breed,
+            "gender": self.gender,
+            "tracker": self.tracker.to_dict()
+            # Add other attributes if needed
+        }
+    def to_json(self):
+        """Convert class attributes to JSON."""
+        return json.dumps(self.to_dict(), indent=4)
--- DogTracker/main.py
+++ DogTracker/main.py
@@ -1,7 +1,126 @@
+import sys
+import os
+import json
 
 from dog import Dog
+from trackers.food.objects.food_entry import FoodEntry
+from trackers.exercise.objects.exercise_activity import ExerciseActivity
+from trackers.vet.objects.vet_visits import VetVisit
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+dog_dict = {}
+
+def dog_exist(dog_name: str):
+    try:
+        value = dog_dict[dog_name]
+        return True
+    except KeyError:
+        print("=" * 10 + "ERROR" + "=" * 10)
+        print(f"There are no dogs in the list.\nPlease add a dog first")
+        return False
+
+def main():
+    while True:
+        # welcome dialog
+        print(f"Welcome to the KAI9 App :-)")
 
 
-while True:
-    # getting user choice to whether they want to sign in or someone new is signing in
-    log_signup_choice = input("Do you want to LOG IN [l] or SIGN UP [s]?:\n")
+        # if user has at least one dog listed then they are not prompted to enter new dog instance
+        action_choice = input(f"What would you like to look at?\n"
+                              "=> Food Menu [f]\n"
+                              "=> Vet Information [v]\n"
+                              "=> Exercise Tracker [e]\n"
+                              "=> Add Your Dogs [d]\n"
+                              "=> Print Tracker Log [s]\n"
+                              "=> Save Info [x]\n"
+                              "=> Quit [q]\n")
+
+        if action_choice == "d":
+            dog_name = input("Enter your dog's name [Only letters]: ")
+            dog_breed = input("Enter your dog's breed: ")
+            dog_gender = input("Enter your dog's gender [M/F]: ")
+            new_dog = Dog(dog_name, dog_breed, dog_gender)
+            if dog_name not in dog_dict:
+                dog_dict[dog_name] = new_dog
+                print("the size of the dictionary is: ", len(dog_dict))
+
+            else:
+                print("=" * 10 + "ERROR" + "=" * 10)
+                print(f"Dog {dog_name} already exists")
+                continue
+
+        elif action_choice == "f":
+            while True:
+                dog_name = input("For which dog do you want to add a food entry for:\n")
+
+                if dog_exist(dog_name):
+                    food_type = input(f"Which type of food?\n")
+                    food_quantity = input(f"How much food did you feed in grams?\n")
+                    feeding_time = input(f"What was the time of feeding?\n"
+                                         f"Morning [m]\n"
+                                         f"Afternoon [a]\n"
+                                         f"Evening [e]\n")
+                    dog = dog_dict.get(dog_name)
+                    new_food_obj = FoodEntry(food_type, food_quantity, feeding_time)
+                    dog.tracker.food_tracker_list.append(new_food_obj)
+                    break
+                else:
+                    print("Dog does not exist, TRY AGAIN")
+                    input("Press Enter to continue...")
+
+        elif action_choice == "v":
+            while True:
+                dog_name = input("For which dog do you want to add a food entry for:\n")
+                if dog_exist(dog_name):
+                    input_visit_date = input(f"What was the date of visit?\n")
+                    input_reason = input(f"What was the reason of visit?\n")
+                    input_outcome = input(f"What was the outcome of visit?\n")
+                    input_vet_name = input(f"What was the name of vet?\n")
+                    dog = dog_dict.get(dog_name)
+                    new_vet_obj = VetVisit(input_visit_date, input_reason, input_outcome, input_vet_name)
+                    dog.tracker.vet_tracker_list.append(new_vet_obj)
+                    break
+                else:
+                    print("Dog does not exist, TRY AGAIN")
+                    input("Press Enter to continue...")
+
+        elif action_choice == "e":
+            while True:
+                dog_name = input("For which dog do you want to add a exercise entry for:\n")
+                if not dog_exist(dog_name):
+                    input_type = input(f"Which type of exercise?\n")
+                    input_intensity = int(input(f"What was the intensity of exercise [1 to 10]?\n"))
+                    input_duration = int(input(f"What was the duration of exercise?\n"))
+                    dog = dog_dict.get(dog_name)
+                    new_exercise_obj = ExerciseActivity(input_type, input_duration, input_intensity)
+                    dog.tracker.vet_tracker_list.append(new_exercise_obj)
+                    break
+                else:
+                    print("Dog does not exist, TRY AGAIN")
+                    input("Press Enter to continue...")
+
+
+        elif action_choice == "s":
+            for dog_name, dog_obj in dog_dict.items():
+                dog_obj.dog_details(dog_name)
+
+        elif action_choice == "q":
+            break
+
+        # Convert dictionary to JSON
+        print("before looping")
+        dog_dict_json = {name: dog.to_dict() for name, dog in dog_dict.items()}
+        print("after looping")
+        json_data = json.dumps(dog_dict_json, indent=4)
+        # Print JSON string
+        print(json_data)
+
+def is_name_unique(dog_list, name):
+    names = [dog.name for dog in dog_list]
+    return names.count(name) == 1
+
+
+# main loop execution on file run
+if __name__ == "__main__":
+    main()
